51b4d68d89ee654b549eb71f98eb5d27af6e12c8
Carol Holmstedt <cjkulzer@gmail.com>
2020-04-29 20:43:07 -0700

    refactor

--- starters/06_tooling/src/roman_numeral_converter.js
+++ starters/06_tooling/src/roman_numeral_converter.js
@@ -1,12 +1,40 @@
 function convert(romanNumeral) {
-    if (romanNumeral === 'i')
-        return 1;
-    else if (romanNumeral === 'x')
-        return 10;
-    else if (romanNumeral === 'xxxiv')
-        return 34;
-    else
-        return 4;
- }
+    let total = 0;
+    let previousValue = 0;
+    const romanNum = romanNumeral.split("");
+    for (let i = 0; i < romanNumeral.length; i++) {
+        let currentValue = toDecimal(romanNum[i]);
+        if (previousValue < currentValue) {
+            // subtracting the previous value twice because
+            // it was added in the previous iteration of the loop
+            total += currentValue - (2 * previousValue);
+        } else {
+            total += currentValue;
+        }
+        previousValue = currentValue;
+    }
+    return total;
+}
 
- export default convert;
+function toDecimal(romanNumeral) {
+    switch (romanNumeral.toLowerCase()) {
+        case 'i':
+            return 1;
+        case 'v':
+            return 5;
+        case 'x':
+            return 10;
+        case 'l':
+            return 50;
+        case 'c':
+            return 100;
+        case 'd':
+            return 500;
+        case 'm':
+            return 1000;
+        default:
+            console.warn("Unknown numeral: " + romanNumeral);
+    }
+}
+
+export default convert;
