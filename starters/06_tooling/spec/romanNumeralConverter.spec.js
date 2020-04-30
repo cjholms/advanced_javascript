262b36554718cfd0d0babbb894fe81802c7bf45a
Carol Holmstedt <cjkulzer@gmail.com>
2020-04-29 20:43:07 -0700

    refactor

--- starters/06_tooling/spec/romanNumeralConverter.spec.js
+++ starters/06_tooling/spec/romanNumeralConverter.spec.js
@@ -1,19 +1,32 @@
 import convert from '../src/roman_numeral_converter'
 
-describe('Roman numeral converter', () => 
-{ it('will return 1 given i', () => {
-    let actual = convert('i');
-    expect(actual).toEqual(1); });
-
-    it('will return 4 given iv', () => {
-    let actual = convert('iv');
-    expect(actual).toEqual(4); });
-
-    it('will return 10 given x', () => {
-        let actual = convert('x');
-        expect(actual).toEqual(10); });
+describe('Roman numeral converter', () => {
+    it('will return 6 given vi', () => {
+        let actual = convert('vi');
+        expect(actual).toEqual(6);
+    });
 
     it('will return 34 given xxxiv', () => {
         let actual = convert('xxxiv');
-        expect(actual).toEqual(34); });
+        expect(actual).toEqual(34);
+    });
+
+    it('will return 49 given xlix', () => {
+        let actual = convert('xlix');
+        expect(actual).toEqual(49);
+    });
+
+    it('will return 478 given cdlxxviii', () => {
+        let actual = convert('cdlxxviii');
+        expect(actual).toEqual(478);
+    });
+
+    it('will return 1990 given mcmxc', () => {
+        let actual = convert('mcmxc');
+        expect(actual).toEqual(1990);
+    });
+
+    it('will return 2016 given mmxvi', () => {
+        let actual = convert('mmxvi');
+        expect(actual).toEqual(2016); });
 });
