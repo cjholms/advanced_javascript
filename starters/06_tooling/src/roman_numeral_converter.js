aecc87060f90edae3f5f85c23029182f077ba1b8
Carol Holmstedt <cjkulzer@gmail.com>
2020-04-29 19:01:11 -0700

    Breakout session

--- starters/06_tooling/src/roman_numeral_converter.js
+++ starters/06_tooling/src/roman_numeral_converter.js
@@ -1,6 +1,10 @@
 function convert(romanNumeral) {
     if (romanNumeral === 'i')
         return 1;
+    else if (romanNumeral === 'x')
+        return 10;
+    else if (romanNumeral === 'xxxiv')
+        return 34;
     else
         return 4;
  }
